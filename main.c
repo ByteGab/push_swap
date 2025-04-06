19ac1b16a79c09d2725b62b9c1eacc383e920b42
ByteGab <gabrielfreiresimon@gmail.com>
2025-04-07 01:48:16 +0200

    move demos

---
+++ main.c
@@ -0,0 +1,150 @@
+// // ---------------------------------------------
+// 		// prueba sa
+// 		sa(stacks, 1);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		// prueba sb
+// 		sb(stacks, 1);
+// 		temp_b = stacks->stack_b;
+// 		while (temp_b != NULL)
+// 		{
+// 			printf("Numero: %d", temp_b->nbr);
+// 			printf("   Indice: %ld", temp_b->index);
+// 			printf("\n");
+// 			temp_b = temp_b->next;
+// 		}
+// 		// prueba ss
+// 		// ss(stacks);
+// 		// ---------------------------------------------
+// 		// prueba pb
+// 		pb(stacks);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		printf("\n");
+// 		temp_b = stacks->stack_b;
+// 		while (temp_b != NULL)
+// 		{
+// 			printf("Numero: %d", temp_b->nbr);
+// 			printf("   Indice: %ld", temp_b->index);
+// 			printf("\n");
+// 			temp_b = temp_b->next;
+// 		}
+// 		// prueba pa
+// 		pa(stacks);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		printf("\n");
+// 		temp_b = stacks->stack_b;
+// 		while (temp_b != NULL)
+// 		{
+// 			printf("Numero: %d", temp_b->nbr);
+// 			printf("   Indice: %ld", temp_b->index);
+// 			printf("\n");
+// 			temp_b = temp_b->next;
+// 		}
+// 		pb(stacks);
+// 		pb(stacks);
+// 		// prueba ra
+// 		ra(stacks, 1);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		// prueba rb
+// 		rb(stacks,1);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		printf("\n");
+// 		temp_b = stacks->stack_b;
+// 		while (temp_b != NULL)
+// 		{
+// 			printf("Numero: %d", temp_b->nbr);
+// 			printf("   Indice: %ld", temp_b->index);
+// 			printf("\n");
+// 			temp_b = temp_b->next;
+// 		}
+// 		// prueba rr
+// 		rr(stacks);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		printf("\n");
+// 		temp_b = stacks->stack_b;
+// 		while (temp_b != NULL)
+// 		{
+// 			printf("Numero: %d", temp_b->nbr);
+// 			printf("   Indice: %ld", temp_b->index);
+// 			printf("\n");
+// 			temp_b = temp_b->next;
+// 		}
+// 		rra(stacks,1);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		pb(stacks);
+// 		rrb(stacks,1);
+// 		temp_b = stacks->stack_b;
+// 		while (temp_b != NULL)
+// 		{
+// 			printf("Numero: %d", temp_b->nbr);
+// 			printf("   Indice: %ld", temp_b->index);
+// 			printf("\n");
+// 			temp_b = temp_b->next;
+// 		}
+// 		rrr(stacks);
+// 		temp_a = stacks->stack_a;
+// 		while (temp_a != NULL)
+// 		{
+// 			printf("Numero: %d", temp_a->nbr);
+// 			printf("   Indice: %ld", temp_a->index);
+// 			printf("\n");
+// 			temp_a = temp_a->next;
+// 		}
+// 		printf("\n");
+// 		temp_b = stacks->stack_b;
+// 		while (temp_b != NULL)
+// 		{
+// 			printf("Numero: %d", temp_b->nbr);
+// 			printf("   Indice: %ld", temp_b->index);
+// 			printf("\n");
+// 			temp_b = temp_b->next;
+// 		}
+// 		exit(0);
